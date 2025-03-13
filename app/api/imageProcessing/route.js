ab509427baa7a84bf37117c7098c5e3e95d7d21b
emil shiju <emilshiju10@gmail.com>
2025-03-13 18:22:49 +0530

    final commit

--- app/api/imageProcessing/route.js
+++ app/api/imageProcessing/route.js
@@ -1,41 +1,29 @@
-import connectToMongo from '@/app/lib/config/db';
-import CaptchaModel from '@/app/lib/models/CaptchaModel';
+import connectToMongo from "@/app/lib/config/db";
+import CaptchaModel from "@/app/lib/models/CaptchaModel";
 
-import { NextResponse } from 'next/server';  // Add this import
+import { NextResponse } from "next/server"; // Add this import
 
-const LoadDB=async()=>{
+const LoadDB = async () => {
+  await connectToMongo();
+};
 
-  await connectToMongo()
-}
-
-LoadDB()
-
-
-
+LoadDB();
 
 export async function POST(req) {
-  
   try {
-
     const data = await req.json();
-    console.log("Received Data:", data.status);  // Log received data
+    console.log("Received Data:", data.status); // Log received data
 
-    if(data.status){
+    if (data.status) {
+      console.log("inside here ");
 
-      console.log("inside here ")
+      const res = await CaptchaModel.create({});
 
-      const res=await CaptchaModel.create({})
-
-      return NextResponse.json({status:true});
-
-
+      return NextResponse.json({ status: true });
     }
-    
-
-
   } catch (error) {
-    console.error("Error in processing the request:", error);  // Log the error message
-    return NextResponse.json({ status:false }, { status: 500 });
+    console.error("Error in processing the request:", error); // Log the error message
+    return NextResponse.json({ status: false }, { status: 500 });
   }
 }
 
@@ -43,20 +31,19 @@ export async function POST(req) {
 
 
 
+export async function GET(req) {
+  try {
+    const data = await CaptchaModel.findOne();
 
-export async function GET(req){
-
-
-  try{
-
-    const data=await CaptchaModel.findOne()
-    
-
-
-    return NextResponse.json({data:data})
-
-
-  }catch(error){
-    console.log(error)
+    if (data?.data.status == true) {
+      await CaptchaModel.deleteMany();
+      return NextResponse.json({ data: data });
+      
+    } else {
+      return NextResponse.json({ data: false });
+    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ data: false });
   }
 }
